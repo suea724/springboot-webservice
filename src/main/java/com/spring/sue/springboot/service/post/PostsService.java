0c6ec3dd37b70733900f97686abdcac510fae821
suea724 <suea724@gmail.com>
2022-06-02 21:16:19 +0900

    게시글 등록 기능 추가 후 테스트

---
+++ src/main/java/com/spring/sue/springboot/service/post/PostsService.java
@@ -0,0 +1,19 @@
+package com.spring.sue.springboot.service.post;
+
+import com.spring.sue.springboot.domain.posts.PostsRepository;
+import com.spring.sue.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
