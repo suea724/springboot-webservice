82431125bdef26cc46d9162bd89524f000c0f997
suea724 <suea724@gmail.com>
2022-06-06 15:08:19 +0900

    mustache로 기본 페이지 만들고 테스트

---
+++ src/test/java/com/spring/sue/springboot/web/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.spring.sue.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void 메인페이지_로딩() {
+        // when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        // then
+        Assertions.assertThat(body).contains("스프링 부트로 시작하는 웹서비스");
+    }
+
+}
